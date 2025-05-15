9ba43bcf9552ed3af269914f843909e77bda6971
Aram <89998809+Aramikboy12@users.noreply.github.com>
2025-05-15 09:14:39 -0700

    Add files via upload

---
+++ ClassCify/typechecker/main_typechecker_error.c
@@ -0,0 +1,34 @@
+#include "../parser/parser.h"
+#include "../tokenizer/tokenizer.h"
+#include "typechecker.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Tokenizer tokenizer;
+
+int main(void)
+{
+    FILE *f = fopen("typechecker_error_test.txt", "r");
+    if (!f)
+    {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
+
+    fseek(f, 0, SEEK_END);
+    long size = ftell(f);
+    rewind(f);
+    char *src = malloc(size + 1);
+    fread(src, 1, size, f);
+    src[size] = '\0';
+    fclose(f);
+
+    init_tokenizer(&tokenizer, src);
+    ASTNode *ast = parse_program();
+
+    typecheck_program(ast);
+
+    free_ast(ast);
+    free(src);
+    return EXIT_SUCCESS;
+}
